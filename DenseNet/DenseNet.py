ddfed67307790b875ac8aadea9ea755b04a05e7a
hupeng <945432954@qq.com>
2019-07-22 17:49:05 +0800

    将pandas中的display.height的代码注释掉了，这是因为pandas中的display.height已经弃用了

--- DenseNet/DenseNet.py
+++ DenseNet/DenseNet.py
@@ -25,7 +25,7 @@ from keras import backend as K
 
 # from denModel import conv_block, dense_block, transition_block
 
-pd.set_option('display.height',1000)
+# pd.set_option('display.height',1000)
 pd.set_option('display.max_rows',500)
 pd.set_option('display.max_columns',500)
 pd.set_option('display.width',1000)
