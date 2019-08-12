3a57606ee0a2e328b452c5d4eded81b206b9e519
hupeng <945432954@qq.com>
2019-08-12 08:28:29 +0800

    更新了Python的学习笔记

--- Python_Learning/plt_test.py
+++ Python_Learning/plt_test.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import warnings
+
+warnings.filterwarnings('ignore')
 
 # 绘制简单的曲线
 # plt.plot([1, 3, 5], [4, 8, 10])
@@ -18,7 +23,7 @@ import numpy as np
 
 # plt.figure(1, dpi= 50) # 创建图表1，dpi代表图片精细度，dpi越大图片文件越大，杂志要300以上
 # data = [1, 1, 1, 2, 2, 2, 3, 3, 4, 5, 5, 6, 4]
-# plt.hist(data) # 只要传入数据，直方图就会统计数据出现的次数
+# plt.hist(data) # 只要传入数据，直方图就会统计数据出现的次数，hist是表示的是直方图
 # plt.show()
 
 # x = np.arange(1, 10)
@@ -27,4 +32,33 @@ import numpy as np
 # plt.scatter(x, y, c= 'r', marker= 'o') # c='r'表示散点的颜色为红色，marker表示指定散点的形状为圆形
 # plt.show()
 
+# 使用Pandas与Matplotlib进行结合
+iris = pd.read_csv('iris_training.csv')
+print(iris.head())
+print(iris.shape)
+print(type(iris))
+print(iris.dtypes)
 
+# 绘制散点图
+# iris.plot(kind = 'scatter', x = '-0.07415843', y = '-0.115833877')
+
+# 没啥用，只是让Pandas的plot()方法在Pycharm上显示
+# plt.show()
+
+# 设置样式
+sns.set(style= 'white', color_codes= True)
+# 设置绘制格式为散点图
+sns.jointplot(x = '120', y = '4', data= iris, height = 5)
+# distplot绘制曲线
+sns.distplot(iris['120'])
+# 没啥用，只是让Pandas的plot()方法在Pycharm上显示
+plt.show()
+
+# FacetGrid 一般绘图函数
+# hue 彩色显示分类 0/1/2
+# plt.scatter 绘制散点图
+# add_legend() 显示分类的描述信息
+# sns.FacetGrid(iris, hue= 'virginica', height= 5).map(plt.scatter, '-0.07415843', '-0.115833877').add_legend()
+# sns.FacetGrid(iris, hue= 'virginica', height= 5).map(plt.scatter, '0.005250311', '-0.062491073').add_legend()
+# 没啥用，只是让Pandas的plot()方法在Pycharm上显示
+# plt.show()
